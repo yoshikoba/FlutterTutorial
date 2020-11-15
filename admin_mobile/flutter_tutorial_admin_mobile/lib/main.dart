a2fc6059a5d753690572d0aab2f5e0f50f1dfa65
yoshikoba <dev.mac.kazukoba@gmail.com>
2020-11-15 17:23:23 +0900

    メインページにtestを追加

---
+++ admin_mobile/flutter_tutorial_admin_mobile/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Mobile',
+      theme: ThemeData.light(),
+      home: AdminMobilePage(),
+    );
+  }
+}
+
+class AdminMobilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SideNavigation(),
+          Container(
+            child: Center(
+              child: Text(
+                'test',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SideNavigation extends StatefulWidget {
+  @override
+  _SideNavigationState createState() => _SideNavigationState();
+}
+
+class _SideNavigationState extends State<SideNavigation> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      destinations: [
+        NavigationRailDestination(
+          icon: Icon(Icons.thumbs_up_down),
+          label: Text('ThumbsUpDown'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.people),
+          label: Text('People'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.face),
+          label: Text('Face'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.bookmark),
+          label: Text('Bookmark'),
+        ),
+      ],
+    );
+  }
+}
